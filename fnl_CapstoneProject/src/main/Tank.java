6659046b9eee813dae05f459caefd4c93fbff803
LoadingL0n3ly <fkhaldin022@student.fuhsd.org>
2022-04-26 19:44:33 -0700

    Implemented Simple Methods For Tank Class

--- fnl_CapstoneProject/src/main/Tank.java
+++ fnl_CapstoneProject/src/main/Tank.java
@@ -22,10 +22,7 @@ public class Tank extends FieldElement {
 	private int votes;
 	private String password;
 	
-	
-
-
-
+	// Tank Constructor
 	public Tank(int x, int y, String name, int power, int shootingRange, int movementRange, int life, int maxLife,
 			int energy, int maxEnergy, int votes, String password, JButton button) {
 		super(x, y, name, button);
@@ -39,6 +36,111 @@ public class Tank extends FieldElement {
 		this.votes = votes;
 		this.password = password;
 	}
+
+	// Tank Upgrades
+	public void upgradePower(int upgradeAmt) {
+		this.power += upgradeAmt;
+	}
+
+	public void upgradeShootingRange(int upgradeAmt) {
+		this.shootingRange += upgradeAmt;
+	}
+
+	public void upgradeMovementRange(int upgradeAmt) {
+		this.movementRange += upgradeAmt;
+	}
+
+	public void upgradeMaxLife(int upgradeAmt) {
+		this.maxLife += upgradeAmt;
+	}
+
+	public void upgradeMaxEnergy(int upgradeAmt) {
+		this.maxEnergy += upgradeAmt;
+	}
+
+	public void upgradeSpecial(int upgradeAmt) {
+		System.out.println("not implemented");
+	}
+
+	// Tank Getters
+	public int getPower() {
+		return power;
+	}
+
+	public int getShootingRange() {
+		return shootingRange;
+	}
+
+	public int getMovementRange() {
+		return movementRange;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public int getMaxLife() {
+		return maxLife;
+	}
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public int getMaxEnergy() {
+		return maxEnergy;
+	}
+
+	public int getVotes() {
+		return votes;
+	}
+
+	public String getType() {
+		System.out.println("Get Type Not Implemented");
+		return "error in get type";
+	}
+
+	public int getSpecial() {
+		System.out.println("Get type not implemented");
+		return 0;
+	}
+
+	// Misc Tank Methods
+
+	public void heal(int healAmt){
+		this.life += healAmt;
+		if(this.life > this.maxLife){
+			this.life = this.maxLife;
+		}
+	}
+
+	public void gainEnergy(int rechargeAmt){
+		this.energy += rechargeAmt;
+		if(this.energy > this.maxEnergy){
+			this.energy = this.maxEnergy;
+		}
+	}
+
+	public void hit(int damage){
+		this.life -= damage;
+		if(this.life < 0){
+			this.life = 0;
+		}
+	}
+
+	public Boolean checkPassword(String password){
+		if(this.password.equals(password)){
+			return true;
+		}
+		else return false;
+	}
+
+	public void upgradeMenu() {
+		System.out.println("not implemented");
+	}
+
+	
+
 
 
     
--- fnl_CapstoneProject/src/tanks/AOE_Tank.java
+++ fnl_CapstoneProject/src/tanks/AOE_Tank.java
@@ -7,7 +7,8 @@ public class AOE_Tank extends Tank {
 
 	public AOE_Tank(int x, int y, String name, int power, int shootingRange, int movementRange, int life, int maxLife,
 			int energy, int maxEnergy, int votes, String password, JButton button) {
-		// TODO Auto-generated constructor stub
+		
+				super(x, y, name, power, shootingRange, movementRange, life, maxLife, energy, maxEnergy, votes, password, button);
 	}
 
 }
