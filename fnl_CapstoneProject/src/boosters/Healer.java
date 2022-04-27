8f1d7444170f99554e6c4d074f65253414bd90c4
Wilson Wu <68243620+wilsonpotato@users.noreply.github.com>
2022-04-27 13:05:43 -0700

    Did some stuff

--- fnl_CapstoneProject/src/boosters/Healer.java
+++ fnl_CapstoneProject/src/boosters/Healer.java
@@ -7,12 +7,6 @@ import java.awt.*;
 import main.*;
 
 public class Healer extends Booster {
-	private int x;
-	private int y;
-	private int strength;
-	private String name;
-	private JButton button;
-	private Color color;
 	public Healer(int x, int y, int strength, String name, JButton button, Color color) {
 		super(x, y,name, strength, button, color);
 	}
--- fnl_CapstoneProject/src/boosters/Shooter.java
+++ fnl_CapstoneProject/src/boosters/Shooter.java
@@ -5,12 +5,6 @@ import java.awt.*;
 import main.*;
 
 public class Shooter extends Booster {
-	private int x;
-	private int y;
-	private int strength;
-	private String name;
-	private JButton button;
-	private Color color;
 	public Shooter(int x, int y, int strength, String name, JButton button, Color color) {
 		super(x, y,name, strength, button, color);
 	}
