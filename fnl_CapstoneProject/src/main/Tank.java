ed847ac362d1ad14f71df0b067b54b0467a23ab5
wilsonpotato <normalwilsonwu@gmail.com>
2022-04-26 19:06:47 -0700

    Revert "added fields"
    This reverts commit 86ff361b0df70ffcb136a350c415821959f57f7c.

--- fnl_CapstoneProject/src/main/Tank.java
+++ fnl_CapstoneProject/src/main/Tank.java
@@ -1,16 +1,4 @@
 package main;
-import java.awt.*;
-public class Tank extends FieldElement {
-    private Color color; 
-    private int shootingRange;
-    private int movementRange;
-    private int life;
-    private int maxLife;
-    private int energy;
-    private int maxEnergy;
-    private int votes;
-    private int password;
-
+public abstract class Tank extends FieldElement {
     
-
 }
